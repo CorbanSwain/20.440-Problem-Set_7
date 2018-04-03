47be1e488a17fda5dfd270c7fb5cb71ea40aab0e
Corban Swain <CorbanSwain@gmail.com>
2018-04-03 12:14:53 -0400

    added plotting for ben-hoch procedure function

--- analysis_script.py
+++ analysis_script.py
@@ -22,28 +22,40 @@ y = np.array([0.10, 0.67, 0.63, 0.87])
 
 # calculate the PCC for each row
 pearson_out = [pearsonr(x_slice, y) for x_slice in X]
-coeff, p_vals = (np.array(ls) for ls in zip(*pearson_out))
+coeff, p_vals = (np.array(lst) for lst in zip(*pearson_out))
+
 
 # benjamani hochberg procedure
-def arg_bh_test(p, q=0.1):
+def argbenhoch(p, q=0.1, plot=False):
     i = rankdata(p, method='ordinal')
     m = len(p)
     bh_crit = q * i / m
-    # plt.plot(i, bh_crit, '.',  i, p_vals, '.')
+    if plot:
+        s = np.argsort(i)
+        plt.plot(i[s], bh_crit[s],  i[s], p_vals[s])
+        plt.xlabel('Rank')
+        plt.ylabel('P Value')
+        plt.xlim([0, m])
+        plt.ylim([0, 1])
+
     try:
-        cutoff_idx = np.max(np.where(p <= bh_crit))
+        cutoff_i = np.max(i[np.where(p <= bh_crit)[0]])
     except ValueError:
         return np.array([])
-    signif_idx = np.where(i <= i[cutoff_idx])[0]
+    signif_idx = np.where(i <= cutoff_i)[0]
     return signif_idx[np.argsort(p_vals[signif_idx])]
 
+
+# calculate with q value
 q = 0.34
-signif_idx = arg_bh_test(p_vals, q)
+signif_idx = argbenhoch(p_vals, q, plot=True)
 
 print('Part 1 - Table')
-fmt = '%10s (%11s), %4s | %5.2f (%5.3f)'
-for idx in signif_idx:
-    print(fmt % (all_data['Protein'][idx],
+fmt = '%2s | %10s (%11s), %4s | %5.2f (%5.3f)'
+for i, idx in enumerate(signif_idx):
+    print(fmt % (i+1, all_data['Protein'][idx],
                  all_data['Residue'][idx],
                  all_data['Time_Point'][idx],
                  coeff[idx], p_vals[idx]))
+
+plt.show()
